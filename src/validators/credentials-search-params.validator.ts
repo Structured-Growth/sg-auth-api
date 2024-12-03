94c0fda560851ce2b610ab4ec557ab80cffe73a4
anton-devline <anton.z@devline.pro>
2024-12-03 10:06:03 +0000

    feat(SG-517): Update authentication flow

--- src/validators/credentials-search-params.validator.ts
+++ src/validators/credentials-search-params.validator.ts
@@ -1,15 +1,19 @@
 import { joi } from "@structured-growth/microservice-sdk";
-import { CommonSearchParamsValidator } from "./common-search-params.validator";
 
 export const CredentialsSearchParamsValidator = joi.object({
-	query: joi
-		.object({
-			provider: joi.string().valid("local", "google").label("Provider"),
-			providerId: joi.string().label("Provider"),
-			status: joi
-				.array()
-				.items(joi.string().valid("verification", "active", "inactive", "archived").required())
-				.label("Status"),
-		})
-		.concat(CommonSearchParamsValidator),
+	query: joi.object({
+		provider: joi.string().valid("local", "google").label("Provider"),
+		providerId: joi.string().label("Provider"),
+		status: joi
+			.array()
+			.items(joi.string().valid("verification", "active", "inactive", "archived").required())
+			.label("Status"),
+		orgId: joi.number().positive().label("Organization ID"),
+		accountId: joi.number().positive().label("Account ID"),
+		id: joi.array().valid(joi.number().positive().required()).label("Entity IDs"),
+		arn: joi.array().valid(joi.string().required()).label("Entity ARNs"),
+		page: joi.number().positive().label("Page"),
+		limit: joi.number().positive().label("Limit"),
+		sort: joi.array().valid(joi.string().required()).label("Sort"),
+	}),
 });
